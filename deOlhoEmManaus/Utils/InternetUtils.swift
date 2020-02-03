dfbd261953d01e10361286d75fa82fdd478ccfc0
Edson Jr <eds_jr@hotmail.com>
2020-02-03 19:35:56 -0400

    [Project]roject][InternetUtils] - Instalados os pods do facebook e do alamofire. Criado a InternetUtils, uma classe que contem metodos que servem para manipular e verificar a conexao do dispositivo com a web.
    Signed-off-by: Edson Jr <eds_jr@hotmail.com>

---
+++ deOlhoEmManaus/Utils/InternetUtils.swift
@@ -0,0 +1,43 @@
+//
+//  InternetUtils.swift
+//  deOlhoEmManaus
+//
+//  Created by Edson  Jr on 03/02/2020.
+//  Copyright © 2020 Edson  Jr. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+
+
+class Connectivity {
+    
+    
+    //Este metodo somente verifica pontualmente se ha alguma conexao com a web ou nao
+    class func isConnectedToInternet() -> Bool {
+        return NetworkReachabilityManager()?.isReachable ?? false
+    }
+    
+    
+    
+    //Este metodo aciona um observador que verifica a todo instante se ha ou nao conexao com a web
+    class func NetworkReachabilityManagerObserver() -> Bool{
+        let TAG = "[Connectivity]: "
+        var isConnected = true
+        let reachabilityManager = NetworkReachabilityManager()
+        reachabilityManager?.startListening()
+        reachabilityManager?.listener = { _ in
+            if let isNetworkReachable = reachabilityManager?.isReachable,
+                isNetworkReachable == false {
+                //Internet NOT Available
+                print(TAG + "WARNING: no internet available....")
+                isConnected = false
+            }
+        }
+        
+        return isConnected
+    }
+    
+    
+}
