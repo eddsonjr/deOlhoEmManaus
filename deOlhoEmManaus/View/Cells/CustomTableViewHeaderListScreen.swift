2404f1e8392dedc525fab52fda598ad2ca0c6775
Edson  Jr <edsonjr@MacBook-Pro-de-Edson.local>
2020-04-23 15:02:32 -0400

    [CustomTableViewListScreen XIB] - Criada a view [.xib] e a controller dela. Implementando a chamada dela agora na viewcontroller
    Signed-off-by: Edson  Jr <edsonjr@MacBook-Pro-de-Edson.local>

---
+++ deOlhoEmManaus/View/Cells/CustomTableViewHeaderListScreen.swift
@@ -0,0 +1,25 @@
+//
+//  CustomTableViewHeaderListScreen.swift
+//  deOlhoEmManaus
+//
+//  Created by Edson  Jr on 23/04/20.
+//  Copyright © 2020 Edson  Jr. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewHeaderListScreen: UITableViewHeaderFooterView {
+    
+    
+    @IBOutlet weak var EventTypeNameLabel: UILabel!
+    
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
