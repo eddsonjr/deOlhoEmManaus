a90e8cdc91de85b18a0f1c571a0de7d1df8d8842
Edson Jr <eddson.jr@gmail.com>
2021-06-04 18:48:29 -0400

    Adicionando uma viewController ao storyboard para criar uma splashscreen
    Signed-off-by: Edson Jr <eddson.jr@gmail.com>

---
+++ deOlhoEmManaus/Controller/SplashScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SplashScreenViewController.swift
+//  deOlhoEmManaus
+//
+//  Created by Edson  Jr on 04/06/21.
+//  Copyright © 2021 Edson  Jr. All rights reserved.
+//
+
+import UIKit
+
+class SplashScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
