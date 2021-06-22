8e1ff81629b1b02daadb5ce42dc587c628e9eaab
Edson Jr <eddson.jr@gmail.com>
2021-06-21 20:06:55 -0400

    Criado o arquivo de classe da CollectionViewCell que ira ter o noem das categorias
    Signed-off-by: Edson Jr <eddson.jr@gmail.com>

---
+++ deOlhoEmManaus/View/Cells/CategoriesSearchCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CategoriesSearchCollectionViewCell.swift
+//  deOlhoEmManaus
+//
+//  Created by Edson  Jr on 21/06/21.
+//  Copyright © 2021 Edson  Jr. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesSearchCollectionViewCell: UICollectionViewCell {
+    
+    
+    
+    
+}
