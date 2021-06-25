cd14ecbb9824093f0c289a1ea31973e205d08cf4
Edson Jr <eddson.jr@gmail.com>
2021-06-24 20:50:37 -0400

    Criado a MapViewController, para gerenciar o mapa da Apple
    Signed-off-by: Edson Jr <eddson.jr@gmail.com>

---
+++ deOlhoEmManaus/Controller/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewController.swift
+//  deOlhoEmManaus
+//
+//  Created by Edson  Jr on 24/06/21.
+//  Copyright © 2021 Edson  Jr. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
